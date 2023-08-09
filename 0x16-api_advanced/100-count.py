1e8b808ea0ea8f4faf6775bc44fdb6468da5a789
Dannie55 <daniel080@gmail.com>
2023-08-09 11:20:14 +0100

    api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,35 +1,77 @@
 #!/usr/bin/python3
-"""Reddits"""
+""" a recursive function that queries the Reddit API, parses the title of all
+hot articles, and prints a sorted count of given keywords (case-insensitive,
+delimited by spaces. Javascript should count as javascript, but java should
+not).
+
+Requirements:
+- Prototype: def count_words(subreddit, word_list)
+- Note: You may change the prototype, but it must be able to be called with
+  just a subreddit supplied and a list of keywords. AKA you can add a counter
+  or anything else, but the function must work without supplying a starting
+  value in the main.
+- If word_list contains the same word (case-insensitive), the final count
+  should be the sum of each duplicate (example below with java)
+- Results should be printed in descending order, by the count, and if the
+  count is the same for separate keywords, they should then be sorted
+  alphabetically (ascending, from A to Z). Words with no matches should be
+  skipped and not printed. Words must be printed in lowercase.
+- Results are based on the number of times a keyword appears, not titles it
+  appears in. java java java counts as 3 separate occurrences of java.
+- To make life easier, java. or java! or java_ should not count as java
+- If no posts match or the subreddit is invalid, print nothing.
+
+NOTE: Invalid subreddits may return a redirect to search results. Ensure that
+      you are NOT following redirects.
+Your code will NOT pass if you are using a loop and not recursively calling
+the function! This can be done with a loop but the point is to use a
+recursive function. :)
+"""
 import requests
-from collections import Counter
+after = None
 
 
-def count_words(subreddit, word_list, after=None):
-    """Recurse"""
-    user_agent = "MyRedditBot/1.0"
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {"after": after} if after else {}
-    headers = {"User-Agent": user_agent}
-    response = requests.get(url, headers=headers, params=params)
+def count_words(subreddit, word_list):
+    """ I will insert this doc later """
+    my_list = recurse(subreddit)
+    my_dict = {}
+
+    if my_list:
+        for word in word_list:
+            my_dict[word] = 0
+
+        for title in my_list:
+            title_split = title.split(" ")
+
+            for iter in title_split:
+                for iter_split in word_list:
+                    if iter.lower() == iter_split.lower():
+                        my_dict[iter_split] += 1
+
+        for key, val in sorted(my_dict.items(),  key=lambda x: x[1],
+                               reverse=True):
+            if val != 0:
+                print("{}: {}".format(key, val))
+
+
+def recurse(subreddit, hot_list=[]):
+    """ I will also insert this doc later """
+    global after
+    headers = {'User-Agent': 'ledbag123'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
     if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        titles = [post["data"]["title"].lower() for post in posts]
-        keywords = [word.lower() for word in word_list]
-        keyword_counter = Counter()
-        for title in titles:
-            for keyword in keywords:
-                if f' {keyword} ' in f' {title} ':
-                    keyword_counter[keyword] += 1
-        if "after" in data["data"] and data["data"]["after"] is not None:
-            return count_words(subreddit,
-                               word_list, after=data["data"]["after"])
-        else:
-            sorted_keywords = sorted(keyword_counter.items(),
-                                     key=lambda item: (-item[1], item[0]))
-            for keyword, count in sorted_keywords:
-                print(f"{keyword}: {count}")
-    elif response.status_code == 404:
-        print("Invalid subreddit or no results found.")
+        prox = response.json().get('data').get('after')
+
+        if prox is not None:
+            after = prox
+            recurse(subreddit, hot_list)
+        list_titles = response.json().get('data').get('children')
+
+        for title_ in list_titles:
+            hot_list.append(title_.get('data').get('title'))
+        return hot_list
     else:
-        print(f"Error: {response.status_code}")
+        return None
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
-"""
-Recursive function that queries the Reddit API and returns
-a list containing the titles of all hot articles for a given subreddit:
+"""recursive function that queries the Reddit API
+and returns a list containing the titles of all hot articles
 """
 import requests
-after = None
 
 
-def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
-    global after
-    user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
+def recurse(subreddit, hot_list=[], after=None, count=0):
+    """Ftn that returns a list containing all hot articles"""
 
-    if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
-            recurse(subreddit, hot_list)
-        all_titles = results.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
-        return hot_list
-    else:
-        return (None)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
+                Safari/537.36 Brave/94.1.23.1 Chrome/94.0.4606.61'
+                }
+
+    params = {
+        "limit": 50,
+        "after": after,
+        "count": count
+    }
+
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers=headers, params=params,
+        allow_redirects=False
+    )
+
+    if response.status_code != 200:
+        return None
+
+    data = response.json().get("data")
+    after = data.get("after")
+    count += data.get("dist")
+
+    for child in data.get("children"):
+        hot_list.append(child.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+
+    return hot_list

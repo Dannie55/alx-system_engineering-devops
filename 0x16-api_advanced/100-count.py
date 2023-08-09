5e91edd9ab68631f38f36940966bdd795902dd16
Dannie55 <daniel080@gmail.com>
2023-08-09 07:27:44 +0100

    api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,50 +1,35 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+"""Reddits"""
 import requests
+from collections import Counter
 
 
-def count_words(subreddit, word_list, count_dict={}, after=None):
-    """Prints counts of given words found in hot posts of a given subreddit.
-
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
-    """
-    params = {"limit": 50}
-    if after:
-        params["after"] = after
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={
-            "User-Agent": "underscoDe@alx-holbertonschool"},
-        params=params,
-        allow_redirects=False)
-    if req.status_code == 200:
-        posts = req.json().get("data").get("children")
-        for post in posts:
-            title = post.get("data").get("title")
-            word_list = [word.lower() for word in word_list]
-            for word in word_list:
-                w_count = title.split().count(word)
-                if count_dict.get(word):
-                    count_dict[word] += w_count
-                else:
-                    count_dict[word] = w_count
-        if req.json().get("data").get("after"):
-            count_words(
-                subreddit,
-                word_list=word_list,
-                count_dict=count_dict,
-                after=req.json().get("data").get("after"))
+def count_words(subreddit, word_list, after=None):
+    """Recurse"""
+    user_agent = "MyRedditBot/1.0"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    params = {"after": after} if after else {}
+    headers = {"User-Agent": user_agent}
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        posts = data["data"]["children"]
+        titles = [post["data"]["title"].lower() for post in posts]
+        keywords = [word.lower() for word in word_list]
+        keyword_counter = Counter()
+        for title in titles:
+            for keyword in keywords:
+                if f' {keyword} ' in f' {title} ':
+                    keyword_counter[keyword] += 1
+        if "after" in data["data"] and data["data"]["after"] is not None:
+            return count_words(subreddit,
+                               word_list, after=data["data"]["after"])
         else:
-            for pair in sorted(
-                    count_dict.items(),
-                    key=lambda kv: (
-                        kv[1],
-                        kv[0]),
-                    reverse=True):
-                if pair[1]:
-                    print("{}: {}".format(pair[0].strip(), pair[1]))
+            sorted_keywords = sorted(keyword_counter.items(),
+                                     key=lambda item: (-item[1], item[0]))
+            for keyword, count in sorted_keywords:
+                print(f"{keyword}: {count}")
+    elif response.status_code == 404:
+        print("Invalid subreddit or no results found.")
+    else:
+        print(f"Error: {response.status_code}")
